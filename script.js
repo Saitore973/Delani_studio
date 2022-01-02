7455769797958549020b18f87ff6e7a43d911801
Saitore973 <saitorevg@gmail.com>
2022-01-02 21:54:22 +0300

    validate forms

--- script.js
+++ script.js
@@ -56,3 +56,55 @@ $(".photo2").hover(() => {
   $(".work2").toggle();
 });
 
+$(".photo3").hover(() => {
+  $(".work3").toggle();
+});
+
+$(".photo4").hover(() => {
+  $(".work4").toggle();
+});
+
+
+$(".photo5").hover(() => {
+  $(".work5").toggle();
+});
+
+
+$(".photo6").hover(() => {
+  $(".work6").toggle();
+});
+
+$(".photo7").hover(() => {
+  $(".work7").toggle();
+});
+
+
+$(".photo8").hover(() => {
+  $(".work8").toggle();
+});
+
+
+function validate() {
+  let name = document.getElementById("contact").value;
+  let mail = document.getElementById("mail").value;
+  let message = document.getElementById("message").value;
+  if (name.length < 2 || name.length === "") {
+    alert("please Enter your full name");
+  }
+
+  if (name.length > 2 && message.length < 10) {
+    alert("meassage too short");
+  }
+  if (name.length > 2 && message.length > 10) {
+    alert(
+      "Hi " +
+        name +
+        " we have received your message. Thank you for keeping in touch"
+    );
+  }
+}
+
+var button = document.getElementById("button");
+button.addEventListener("click", () => {
+  validate();
+});
